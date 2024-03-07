73843fa4f2768b2c2b4be3de6b727f718858eac4
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-07 17:35:12 +0600

    problem 6

--- problem(06).js
+++ problem(06).js
@@ -1,1 +1,12 @@
 // Input 3 numbers and print a maximum between three numbers
+let num1 = parseInt(prompt("Enter first number"));
+let num2 = parseInt(prompt("Enter second number"));
+let num3 = parseInt(prompt("Enter third number"));
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1);
+} else if ((num2 > num1) & (num2 > num3)) {
+  console.log(num2);
+} else {
+  console.log(num3);
+}
