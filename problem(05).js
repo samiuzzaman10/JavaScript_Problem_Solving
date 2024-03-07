02c50716d3ca1c6ba6693de6ba0978fb154aa8c6
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-07 17:28:53 +0600

    problem 5

--- problem(05).js
+++ problem(05).js
@@ -1,1 +1,8 @@
 // Determine Whether a number is even or odd
+
+let input = parseInt(prompt("Enter a Number: "));
+if (input % 2 == 0) {
+    console.log("Number is Even");
+} else {
+    console.log("Number is Odd")
+}
