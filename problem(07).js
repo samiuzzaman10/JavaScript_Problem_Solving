776cdb1ec169210ed833dbfe9432949e7c86e407
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-07 17:38:56 +0600

    problem 7

--- problem(07).js
+++ problem(07).js
@@ -1,1 +1,10 @@
 // Check whether a number is positive, negative of zero
+let number = parseInt(prompt("Enter a number: "));
+
+if (number > 0) {
+    console.log("Number is Positive");
+} else if (number < 0) {
+    console.log("Number is Negative");
+} else {
+    console.log("Number is Zero")
+}
