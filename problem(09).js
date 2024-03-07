e018f5bbe8ecd5504c75ddcb196f2cdff3f1a705
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-07 18:19:54 +0600

    problem 9

--- problem(09).js
+++ problem(09).js
@@ -1,1 +1,9 @@
-// Solve this code- marks = int(input(4.67)) if marks >= 80: print("very good") elif marks >= 50 and marks < 80: print("good") else: print("need to improve")
+// Take input letter and check whether letter is vowel or consonant
+
+let lette = prompt("Enter a letter");
+let letter = lette.toUpperCase();
+if (letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U") {
+    console.log("Vowel");
+} else {
+    console.log("Consonant");
+}
