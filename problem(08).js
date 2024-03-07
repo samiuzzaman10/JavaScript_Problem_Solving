64e4d672acd1c81732763358beaf202a8992f546
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-07 17:44:30 +0600

    problem 8

--- problem(08).js
+++ problem(08).js
@@ -1,1 +1,9 @@
  // Input two numbers and print their sum only if the sum is greater than 100
+
+let num1 = parseInt(prompt("Enter First Number: "));
+let num2 = parseInt(prompt("Enter Last Number: "));
+let sum = num1 + num2;
+
+if (sum >= 100) {
+    console.log(num1 + "+" + num2 + "=" + sum);
+}
