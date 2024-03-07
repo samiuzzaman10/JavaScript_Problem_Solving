86cebd211c78c3d34ac0a6d25c652f14edd4ecab
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-07 17:12:45 +0600

    Problem 3

---
+++ problem(03).js
@@ -0,0 +1,20 @@
+// Leter Grade: input user exam mark and show the output CGPA.
+
+let marks = prompt("Enter Your Marks: ");
+if (marks >= 80 && marks <= 100) {
+  console.log("A+");
+} else if (marks >= 70 && marks <= 79) {
+  console.log("A");
+} else if (marks >= 60 && marks <= 69) {
+  console.log("A-");
+} else if (marks >= 50 && marks <= 59) {
+  console.log("B");
+} else if (marks >= 40 && marks <= 49) {
+  console.log("C");
+} else if (marks >= 30 && marks <= 39) {
+  console.log("D");
+} else if (marks >= 0 && marks < 33) {
+  console.log("Fail");
+} else {
+  console.log("Invalid Marks");
+}
