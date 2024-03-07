bb9b85473c8f0646be3ecef3006d76c105d1bc30
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-08 00:32:19 +0600

    problem 13

--- problem(13).js
+++ problem(13).js
@@ -1,1 +1,6 @@
-//  Print the elements of a list using a for loop:
+//  Print the elements of a Array using a for loop:
+
+let language = ["JavaScript", "Python", "Php", "Java", "C"];
+for (i = 0; i <= 4; i++) {
+    console.log(i + " " + language[i]);
+}
