1ec3957a0b4889071d9f0eea77413475eb874358
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-08 01:17:22 +0600

    problem 20

--- problem(20).js
+++ problem(20).js
@@ -1,1 +1,17 @@
 // Display Fibonacci series up to 10 terms
+function fibonacci(x) {
+  var a = 0;
+  var b = 1;
+  console.log(a);
+  console.log(b);
+
+  var sum = 0;
+  for (i = 1; i <= x; i++) {
+    sum = a + b
+    c = a + b;
+    a = b;
+    b = c;
+    console.log(sum);
+  }
+}
+fibonacci(10);
