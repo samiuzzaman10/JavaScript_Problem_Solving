76a30abdf9cf0ff90168133df73c65288d86b3e0
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-08 18:01:08 +0600

    problem 26

--- problem(26).js
+++ problem(26).js
@@ -1,1 +1,13 @@
-// Write a javascript function to sum all the numbers in a list
+// Write a javascript function to sum all the numbers in a Array
+
+function myFunction(n) {
+    let sum = 0;
+    for (let i = 0; i <= 4; i++) {
+        sum = sum + n[i];
+    }
+    return sum
+}
+
+let numbers = [1, 2, 3, 4, 5];
+result = myFunction(numbers);
+console.log(result)
