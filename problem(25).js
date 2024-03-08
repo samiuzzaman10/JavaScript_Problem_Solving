021fb04a98534007bfb7e3567df7ee426d07a200
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-08 16:41:47 +0600

    problem 25

--- problem(25).js
+++ problem(25).js
@@ -1,1 +1,13 @@
 // Write a javascript function to find Max of three numbers.
+
+function maxNumber(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+result = maxNumber(1, 2, 3);
+console.log(result);
