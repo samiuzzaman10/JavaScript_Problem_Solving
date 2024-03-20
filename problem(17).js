5539c0b3adda84a8d6d95169c61809d9a61d8cc5
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-20 22:46:14 +0600

    problem-17

--- problem(17).js
+++ problem(17).js
@@ -1,1 +1,13 @@
-// Print all uppercase letters in a string using a for loop:
+// How can we arrange the names of everyone in our Australian Team Players alphabetically?
+
+let names = [
+  "Josh Hazlewood",
+  "Mitchell Starc",
+  "David Warner",
+  "Steve Smith",
+  "Pat Cummins",
+  "Adam Zampa",
+  "Glenn Maxwell",
+];
+names.sort();
+console.log(names);
