6353f7db6dd7387147820c7532865834d287a736
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-22 21:26:01 +0600

    problem 29

--- problem(29).js
+++ problem(29).js
@@ -1,1 +1,11 @@
 // Write a javascript funtion to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument.
+
+function factorialize(num) {
+  if (num === 0 || num === 1) return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+const result = factorialize(5);
+console.log(result);
