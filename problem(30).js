29b8b70fcf35e3fe19509b28a04ce7cc24f90464
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-22 21:50:00 +0600

    problem 30

--- problem(30).js
+++ problem(30).js
@@ -1,1 +1,21 @@
 // Write a javascript function that accepts a string and calculates the number of uppercase letter and lowercase latters.
+
+const letter = "My Name is Samiuzzaman RAHAT";
+
+let uppercaseLetter = 0;
+let lowercaseLetter = 0;
+
+function calculateString(text) {
+  for (let i = 0; i < letter.length; i++) {
+    console.log(text[i]);
+    if (text[i] >= "A" && text[i] <= "Z") {
+        uppercaseLetter += 1;
+    } else {
+        lowercaseLetter += 1;
+    }
+  }
+}
+calculateString(letter);
+
+console.log(`Uppercase Letter = ${uppercaseLetter}
+Lowercase Letter = ${lowercaseLetter}`);
