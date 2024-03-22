77363e965ca129fc4c38c0258451f579697a2570
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-22 22:00:12 +0600

    problem 31

--- problem(31).js
+++ problem(31).js
@@ -1,1 +1,11 @@
 // Write a javascript function that takes a list and return a new list with unique elements of the list.
+let lists = [3, 5, 8, 3, 9, 5, 4,4];
+function uniqueElement(lst) {
+    let unique = lst.reduce(function(acc, curr){
+        if(!acc.includes(curr)) acc.push(curr);
+        return acc;
+    }, []);
+    return unique;
+}
+const result = uniqueElement(lists);
+console.log(result);
