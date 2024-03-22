6d83bc75c8230d43ead607a9c52112bf25caaf26
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-22 21:02:11 +0600

    problem 27

--- problem(27).js
+++ problem(27).js
@@ -1,1 +1,11 @@
 // Write a javascript function to multiply all the numbers in a list.
+const score = [5, 7, 3, 9, 4];
+let mul = 1;
+function multiplyFunc(n) {
+  for (let i = 0; i < score.length; i++) {
+    mul = mul * n[i];
+  }
+  return mul;
+}
+const result = multiplyFunc(score);
+console.log(result);
